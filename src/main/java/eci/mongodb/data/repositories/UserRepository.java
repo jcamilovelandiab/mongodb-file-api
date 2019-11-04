99ae946301009e43dfa2fc9f8391e3c16b3dedba
jcamilovelandiab <jcamilovelandiab@gmail.com>
2019-11-04 17:29:43 -0500

    fixing

--- src/main/java/eci/mongodb/data/repositories/UserRepository.java
+++ src/main/java/eci/mongodb/data/repositories/UserRepository.java
@@ -3,5 +3,10 @@ package eci.mongodb.data.repositories;
 import eci.mongodb.data.model.User;
 import org.springframework.data.mongodb.repository.MongoRepository;
 
+
 public interface UserRepository extends MongoRepository<User, String> {
+
+
+    User findBy_id(String _id);
+
 }
